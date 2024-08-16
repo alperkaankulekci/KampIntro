29c792059bef2ac9330b018caf4216c6527fcd4f
Alper <alperkaankulekci@gmail.com>
2024-08-16 17:14:51 +0300

    3. gün videosu bittti,

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -4,11 +4,13 @@
     {
         static void Main(string[] args)
         {
+
             //int, decimal, float, double, bool = değer tip
             //değer tip değeri atayıp bırakır.
 
             //array, class, interface = referans tip
             //sadece adresi atar. değeri atamaz.
+
 
             //https://www.youtube.com/watch?v=1j68gb1-qOw&list=PLqG356ExoxZVN7rC0KmMo0lvECK97VRZg&index=6
             //unutursan bak
